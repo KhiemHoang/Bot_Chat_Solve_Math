b55847b2b4d9ef6c12a6d61891cf6af307eb3707
KhiemHoang <51252182+KhiemHoang@users.noreply.github.com>
2020-12-13 22:06:12 +0700

    huhu

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import train_model as model
 
 
 if __name__ == '__main__':
-    annotator = VnCoreNLP("VnCoreNLP\VnCoreNLP-1.1.1.jar", annotators="wseg, pos", max_heap_size='-Xmx2g')
+    #annotator = VnCoreNLP("VnCoreNLP\VnCoreNLP-1.1.1.jar", annotators="wseg, pos", max_heap_size='-Xmx2g')
 
     #text = 'Jack có 15 trái bóng tròn màu xanh vào ngày hôm qua. Kelly có 5 trái bóng tròn màu xanh. Vậy Jack và Kelly có bao nhiêu trái bóng?'
     # text = 'Cậu bé có 5 bó củi. Cậu bé bị mất 2 bó củi nữa. Hỏi cậu bé có bao nhiêu bó củi?'
@@ -22,6 +22,6 @@ if __name__ == '__main__':
     # elif math_type == 'increase' or math_type == 'decrease':
     #     increase_decrease.solve_math_problem(annotator, text, math_type)
     
-    model.dump_file(annotator)
+    model.train_and_test()
 
-    annotator.close()
+    #annotator.close()
--- train_model.py
+++ train_model.py
@@ -1,3 +1,11 @@
+from sklearn import model_selection, preprocessing, metrics, svm
+from sklearn.decomposition import TruncatedSVD
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.model_selection import train_test_split
+
+from keras import layers, models, optimizers
+from keras.layers import *
+
 import vietnamese_nlp as vnlp
 import test_of_test as test
 import pickle
@@ -18,3 +26,82 @@ def dump_file(annotator):
     pickle.dump(X_train, open('data/X_train.pkl', 'wb'))
     pickle.dump(y_train, open('data/y_train.pkl', 'wb'))
 
+def load_train_file():
+    #get data for training
+    X_train = pickle.load(open('data/X_train.pkl', 'rb'))
+    y_train = pickle.load(open('data/y_train.pkl', 'rb'))
+
+    return X_train, y_train
+
+def load_test_file():
+    #get data for testing
+    X_test = pickle.load(open('data/X_test.pkl', 'rb'))
+    y_test = pickle.load(open('data/y_test.pkl', 'rb'))
+
+    return X_test, y_test
+
+def word_vertorize(X_data):
+    tfidf_vect = TfidfVectorizer(analyzer='word')
+    tfidf_vect.fit(X_data)
+
+    X_data_tfidf =  tfidf_vect.transform(X_data)
+
+    svd = TruncatedSVD(n_components=10, random_state=42)
+    svd.fit(X_data_tfidf)
+
+    X_data_tfidf_svd = svd.transform(X_data_tfidf)
+
+    return X_data_tfidf_svd
+
+
+#train model
+def train_model(classifier, X_data, y_data, X_test, y_test, is_neuralnet=False, n_epochs=3):       
+    X_train, X_val, y_train, y_val = train_test_split(X_data, y_data, test_size=0.1, random_state=42)
+    
+    if is_neuralnet:
+        classifier.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=n_epochs, batch_size=512)
+        
+        val_predictions = classifier.predict(X_val)
+        test_predictions = classifier.predict(X_test)
+        val_predictions = val_predictions.argmax(axis=-1)
+        test_predictions = test_predictions.argmax(axis=-1)
+    else:
+        classifier.fit(X_train, y_train)
+    
+        train_predictions = classifier.predict(X_train)
+        val_predictions = classifier.predict(X_val)
+        test_predictions = classifier.predict(X_test)
+        
+    print("Validation accuracy: ", metrics.accuracy_score(val_predictions, y_val))
+    print("Test accuracy: ", metrics.accuracy_score(test_predictions, y_test))
+
+#lstm model
+def create_lstm_model():
+    input_layer = Input(shape=(10,))
+    
+    layer = Reshape((1, 10))(input_layer)
+    layer = LSTM(128, activation='relu')(layer)
+    layer = Dense(512, activation='relu')(layer)
+    layer = Dense(512, activation='relu')(layer)
+    layer = Dense(128, activation='relu')(layer)
+    
+    output_layer = Dense(10, activation='softmax')(layer)
+    
+    classifier = models.Model(input_layer, output_layer)
+    
+    classifier.compile(optimizer=optimizers.Adam(), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    
+    return classifier
+
+def train_and_test():
+    X_train, y_train = load_train_file()
+    X_test, y_test = load_test_file()
+
+    encoder = preprocessing.LabelEncoder()
+    y_data_n = encoder.fit_transform(y_train)
+    y_test_n = encoder.fit_transform(y_test)
+
+    classifier = create_lstm_model()
+    X_data_tfidf_svd = word_vertorize(X_train)
+    X_test_tfidf_svd = word_vertorize(X_test)
+    train_model(classifier=classifier, X_data=X_data_tfidf_svd, y_data=y_data_n, X_test=X_test_tfidf_svd, y_test=y_test_n, is_neuralnet=True)
