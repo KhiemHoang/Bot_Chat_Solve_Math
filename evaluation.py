91a399520ac1e898f86b952ff96ac070b75cdb50
KhiemHoang <51252182+KhiemHoang@users.noreply.github.com>
2020-12-23 22:56:35 +0700

    hihi

---
+++ evaluation.py
@@ -0,0 +1,253 @@
+from sklearn import model_selection, preprocessing, metrics, svm
+from sklearn.decomposition import TruncatedSVD
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.model_selection import train_test_split
+# from sklearn.naive_bayes import MultinomialNB
+
+from keras import layers, models, optimizers
+from keras.layers import *
+
+import vietnamese_nlp as vnlp
+import pickle
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+# import
+
+def dump_file(annotator):
+    # get data for trainning
+    df_train = pd.DataFrame()
+    df_train = pd.read_csv("data/train_data.csv")
+    X_train, y_train = vnlp.pre_process_train_data(annotator,df_train)
+    pickle.dump(X_train, open('data/X_train.pkl', 'wb'))
+    pickle.dump(y_train, open('data/y_train.pkl', 'wb'))
+    print ('Done train')
+
+    # get data for testing
+    df_test = pd.DataFrame()
+    df_test = pd.read_csv("data/test_data.csv")
+    X_test, y_test = vnlp.pre_process_train_data(annotator, df_test)
+    pickle.dump(X_test, open('data/X_test.pkl', 'wb'))
+    pickle.dump(y_test, open('data/y_test.pkl', 'wb'))
+    print ('Done test')
+
+def load_train_file():
+    #get data for training
+    X_train = pickle.load(open('data/X_train.pkl', 'rb'))
+    y_train = pickle.load(open('data/y_train.pkl', 'rb'))
+
+    return X_train, y_train
+
+def load_test_file():
+    #get data for testing
+    X_test = pickle.load(open('data/X_test.pkl', 'rb'))
+    y_test = pickle.load(open('data/y_test.pkl', 'rb'))
+
+    return X_test, y_test
+
+def word_vertorize(X_data):
+    tfidf_vect = TfidfVectorizer(analyzer='word')
+    tfidf_vect.fit(X_data)
+
+    X_data_tfidf =  tfidf_vect.transform(X_data)
+
+    svd = TruncatedSVD(n_components=100, random_state=42)
+    svd.fit(X_data_tfidf)
+
+    X_data_tfidf_svd = svd.transform(X_data_tfidf)
+
+    return X_data_tfidf_svd
+
+
+#train model
+def train_model(classifier, X_data, y_data, X_test, y_test, is_neuralnet, n_epochs=30):       
+    X_train, X_val, y_train, y_val = train_test_split(X_data, y_data, test_size=0.3, random_state=42)
+    
+    # history = classifier.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=n_epochs, batch_size=512)
+    if is_neuralnet:
+        classifier.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=n_epochs, batch_size=512)
+        
+        val_predictions = classifier.predict(X_val)
+        test_predictions = classifier.predict(X_test)
+        val_predictions = val_predictions.argmax(axis=-1)
+        test_predictions = test_predictions.argmax(axis=-1)
+    else:
+        classifier.fit(X_train, y_train)
+    
+        train_predictions = classifier.predict(X_train)
+        val_predictions = classifier.predict(X_val)
+        test_predictions = classifier.predict(X_test)
+    
+    print("Validation accuracy: ", metrics.accuracy_score(val_predictions, y_val))
+    print("Test accuracy: ", metrics.accuracy_score(test_predictions, y_test))
+
+    # plt.plot(history.history['accuracy'])
+    # plt.plot(history.history['val_accuracy'])
+    # plt.title('model accuracy')
+    # plt.ylabel('accuracy')
+    # plt.xlabel('epoch')
+    # plt.legend(['train', 'test'], loc='upper left')
+    # plt.show()
+
+    # plt.plot(history.history['loss'])
+    # plt.plot(history.history['val_loss'])
+    # plt.title('model loss')
+    # plt.ylabel('loss')
+    # plt.xlabel('epoch')
+    # plt.legend(['train', 'test'], loc='upper left')
+    # plt.show()
+
+#lstm model
+def create_lstm_model():
+    input_layer = Input(shape=(100,))
+    
+    layer = Reshape((10, 10))(input_layer)
+    layer = LSTM(128, activation='relu')(layer)
+    layer = Dense(512, activation='relu')(layer)
+    layer = Dense(512, activation='relu')(layer)
+    layer = Dense(128, activation='relu')(layer)
+    
+    output_layer = Dense(5, activation='softmax')(layer)
+    
+    classifier = models.Model(input_layer, output_layer)
+    classifier.summary()
+    classifier.compile(optimizer=optimizers.Adam(), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    
+    return classifier
+
+def create_dnn_model():
+    input_layer = Input(shape=(100,))
+    layer = Dense(1024, activation='relu')(input_layer)
+    layer = Dense(1024, activation='relu')(layer)
+    layer = Dense(512, activation='relu')(layer)
+    output_layer = Dense(10, activation='softmax')(layer)
+    
+    classifier = models.Model(input_layer, output_layer)
+    classifier.compile(optimizer=optimizers.Adam(), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    
+    return classifier
+
+def create_gru_model():
+    input_layer = Input(shape=(100,))
+    
+    layer = Reshape((10, 10))(input_layer)
+    layer = GRU(128, activation='relu')(layer)
+    layer = Dense(512, activation='relu')(layer)
+    layer = Dense(512, activation='relu')(layer)
+    layer = Dense(128, activation='relu')(layer)
+    
+    output_layer = Dense(10, activation='softmax')(layer)
+    
+    classifier = models.Model(input_layer, output_layer)
+    
+    classifier.compile(optimizer=optimizers.Adam(), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    
+    return classifier
+
+def create_brnn_model():
+    input_layer = Input(shape=(100,))
+    
+    layer = Reshape((10, 10))(input_layer)
+    layer = Bidirectional(GRU(128, activation='relu'))(layer)
+    layer = Dense(512, activation='relu')(layer)
+    layer = Dense(512, activation='relu')(layer)
+    layer = Dense(128, activation='relu')(layer)
+    
+    output_layer = Dense(10, activation='softmax')(layer)
+    
+    classifier = models.Model(input_layer, output_layer)
+    
+    classifier.compile(optimizer=optimizers.Adam(), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    
+    return classifier
+
+  
+def create_gru_model():
+    input_layer = Input(shape=(100,))
+    
+    layer = Reshape((10, 10))(input_layer)
+    layer = GRU(128, activation='relu')(layer)
+    layer = Dense(512, activation='relu')(layer)
+    layer = Dense(512, activation='relu')(layer)
+    layer = Dense(128, activation='relu')(layer)
+    
+    output_layer = Dense(10, activation='softmax')(layer)
+    
+    classifier = models.Model(input_layer, output_layer)
+    
+    classifier.compile(optimizer=optimizers.Adam(), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    
+    return classifier
+  
+
+def create_rcnn_model():
+    input_layer = Input(shape=(100,))
+    
+    layer = Reshape((10, 10))(input_layer)
+    layer = Bidirectional(GRU(128, activation='relu', return_sequences=True))(layer)    
+    layer = Convolution1D(100, 3, activation="relu")(layer)
+    layer = Flatten()(layer)
+    layer = Dense(512, activation='relu')(layer)
+    layer = Dense(512, activation='relu')(layer)
+    layer = Dense(128, activation='relu')(layer)
+    
+    output_layer = Dense(10, activation='softmax')(layer)
+    
+    classifier = models.Model(input_layer, output_layer)
+    classifier.summary()
+    classifier.compile(optimizer=optimizers.Adam(), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    
+    return classifier
+
+def train_and_test():
+    X_train, y_train = load_train_file()
+    X_test, y_test = load_test_file()
+
+    encoder = preprocessing.LabelEncoder()
+    y_data_n = encoder.fit_transform(y_train)
+    y_test_n = encoder.fit_transform(y_test)
+
+    X_data_tfidf_svd = word_vertorize(X_train)
+    X_test_tfidf_svd = word_vertorize(X_test)
+
+
+    classifier = create_lstm_model()
+    train_model(classifier, X_data=X_data_tfidf_svd, y_data=y_data_n, X_test=X_test_tfidf_svd, y_test=y_test_n, is_neuralnet=True)
+    
+    # classifier = create_dnn_model()
+    # train_model(classifier=classifier, X_data=X_data_tfidf_svd, y_data=y_data_n, X_test=X_test_tfidf_svd, y_test=y_test_n, is_neuralnet=True)
+
+    # train_model(naive_bayes.MultinomialNB(), X_data_tfidf_svd, y_data_n, X_test_tfidf_svd, y_test_n, is_neuralnet=False)
+
+    # classifier = create_brnn_model()
+    # train_model(classifier=classifier, X_data=X_data_tfidf_svd, y_data=y_data_n, X_test=X_test_tfidf_svd, y_test=y_test_n, is_neuralnet=True)
+
+
+    # classifier = create_gru_model()
+    # train_model(classifier=classifier, X_data=X_data_tfidf_svd, y_data=y_data_n, X_test=X_test_tfidf_svd, y_test=y_test_n, is_neuralnet=True)
+
+    # classifier = create_rcnn_model()
+    # train_model(classifier=classifier, X_data=X_data_tfidf_svd, y_data=y_data_n, X_test=X_test_tfidf_svd, y_test=y_test_n, is_neuralnet=True, n_epochs=20)
+
+
+    # classifier.save('MyModel_v2.h5')
+
+    # text = 'Chú có 1 quyển vở. Chú bị mất 1 quyển vở . Hỏi Chú còn bao nhiêu quyển vở?'
+
+    # tfidf_vect = TfidfVectorizer(analyzer='word')
+    # tfidf_vect.fit(X_train)
+    # X_data_tfidf =  tfidf_vect.transform(X_train)
+
+    # svd = TruncatedSVD(n_components=70, random_state=42)
+    # svd.fit(X_data_tfidf)   
+    # X_data_tfidf_svd = svd.transform(X_data_tfidf)
+
+    # encoder = preprocessing.LabelEncoder()
+    # y_data_n = encoder.fit_transform(y_train)
+
+    # test_doc = vnlp.preprocessing_prediction(annotator, text)
+    # test_doc_tfidf = tfidf_vect.transform([text])    
+    # test_doc_svd = svd.transform(test_doc_tfidf)
+    # print('ok')
+    # classifier.predict(test_doc_svd)
--- main.py
+++ main.py
@@ -1,41 +1,42 @@
-from vncorenlp import VnCoreNLP
-import change_in_out
+# from vncorenlp import VnCoreNLP
+# import change_in_out
 # import combine
 # import increase_decrease
 # import train_model as model
-import predict
+# import predict
 # import data.create_data as create_data
 # import fasttext as ft
 # import pickle
 # import result
+import evaluation
 
 
 if __name__ == '__main__':
-    annotator = VnCoreNLP("VnCoreNLP\VnCoreNLP-1.1.1.jar", annotators="wseg, pos", max_heap_size='-Xmx2g')
+    # annotator = VnCoreNLP("VnCoreNLP\VnCoreNLP-1.1.1.jar", annotators="wseg, pos", max_heap_size='-Xmx2g')
 
     # text = 'Jack có 15 trái bóng tròn màu xanh vào ngày hôm qua. Kelly có 5 trái bóng tròn màu xanh. Vậy Jack và Kelly có bao nhiêu trái bóng?'
     # text = 'Cậu bé có 5 bó củi. Cậu bé bị mất 2 bó củi nữa. Hỏi cậu bé có bao nhiêu bó củi?'
     # text = 'Hiếu sở hữu 6 quả chuối. Vy sở hữu 71 quả chuối. Hiếu giữ 24 quả chuối từ Vy. Hỏi Hiếu và Vy có bao nhiêu quả chuối?'
     # math_type = 'change_in'
     
-    text = 'An giữ 30 viên bi. Tuấn giữ 84 viên bi. An bán 25 viên bi cho Tuấn. Hỏi mỗi người có bao nhiêu viên bi?'
-    math_type = predict.predict_math_type(annotator, text)
+    # text = 'An giữ 30 viên bi. Tuấn giữ 84 viên bi. An bán 25 viên bi cho Tuấn. Hỏi mỗi người có bao nhiêu viên bi?'
+    # math_type = predict.predict_math_type(annotator, text)
 
-    if math_type == 'change_out' or math_type == 'change_in':
-        change_in_out.solve_math_problem(annotator, text, math_type)
+    # if math_type == 'change_out' or math_type == 'change_in':
+    #     change_in_out.solve_math_problem(annotator, text, math_type)
     # elif math_type == 'combine':
     #     combine.solve_math_problem(annotator, text, math_type)
     # elif math_type == 'increase' or math_type == 'decrease':
     #     increase_decrease.solve_math_problem(annotator, text, math_type)
 
-
+    
     # model.dump_file(annotator)
     # model.train_and_test()
     # X_data = pickle.load(open('data/X_test.pkl', 'rb'))
     # X_test = pickle.load(open('data/X_test.pkl', 'rb'))
     # ft.fstxt(annotator, X_data, X_test)
-    annotator.close()
-
+    # annotator.close()
+    evaluation.train_and_test()
 
     # result.draw_label()
 
